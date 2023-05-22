cd46820d634bf648ca6812752777df1e8482bdaf
Edgar Arroyo <edgararroyo306@gmail.com>
2023-05-21 22:19:59 -0600

    Create connection to a song_recor table in the DB

--- model/admin_dao.py
+++ model/admin_dao.py
@@ -2,7 +2,7 @@ from model.db_conection import ConnectDB
 from tkinter import messagebox
 
 
-def create_table():
+def create__directory_table():
     connect = ConnectDB()
 
     sql = '''
@@ -64,3 +64,68 @@ def get_directory():
     directory = directory[:-1]
 
     return directory
+
+
+def create_songs_table():
+    connect = ConnectDB()
+
+    sql = '''
+    CREATE TABLE IF NOT EXISTS songs_record(
+    song_id INTEGER,
+    song_name VARCHAR(100),
+    PRIMARY KEY(song_id AUTOINCREMENT)
+    )
+    '''
+
+    connect.cursor.execute(sql)
+    connect.close()
+
+
+def song_existance(song):
+    connect = ConnectDB()
+
+    sql = """SELECT song_name FROM songs_record"""
+
+    connect.cursor.execute(sql)
+    song_name = connect.cursor.fetchall()
+    connect.close()
+
+    song_name = str(song_name)
+    song_name = song_name.strip("[]")
+
+    if song in song_name:
+        return True
+    return False
+
+
+def save_songs(songs):
+    connect = ConnectDB()
+
+    for song in songs:
+        if song_existance(song) is False:
+            sql = f""" INSERT INTO songs_record (song_name) VALUES ('{song}')"""
+            connect.cursor.execute(sql)
+            connect.close()
+
+
+def get_song():
+    connect = ConnectDB()
+
+    sql = """SELECT song_name FROM songs_record"""
+
+    connect.cursor.execute(sql)
+    song_name = connect.cursor.fetchall()
+    connect.close()
+
+    def iterate_song(song_name):
+        result = []
+        for song in song_name:
+            song = str(song)
+            song = song.strip("()")
+            song = song.strip(",")
+            song = song.strip("'")
+            result.append(song)
+        return result
+
+    list_songs = iterate_song(song_name)
+    return list_songs
