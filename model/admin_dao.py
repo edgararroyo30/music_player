e0c0f7dd5ef2d48c83115162108588002d2e8465
Edgar Arroyo <edgararroyo306@gmail.com>
2023-05-23 19:22:23 -0600

    Creating playing next and back tables and logic, missing some adjustments

--- model/admin_dao.py
+++ model/admin_dao.py
@@ -271,3 +271,148 @@ def add_to_queue():
             sql_add = f'''INSERT INTO queue(song_in_queue) VALUES ('{song}')'''
             connect.cursor.execute(sql_add)
         connect.close()
+
+
+def get_queue_songs_id():
+    connect = ConnectDB()
+
+    sql = '''SELECT id FROM queue'''
+
+    connect.cursor.execute(sql)
+    queue = connect.cursor.fetchall()
+    connect.close()
+
+    return queue
+
+
+def get_playing_song_id(song_name):
+    connect = ConnectDB()
+
+    sql = f''' SELECT id FROM queue WHERE song_in_queue = '{song_name}' '''
+    connect.cursor.execute(sql)
+    song_id = connect.cursor.fetchone()
+    connect.close()
+
+    return song_id
+
+
+def get_playing_next_song(id):
+    connect = ConnectDB()
+
+    sql = f''' SELECT song_in_queue FROM queue WHERE id = '{id}' '''
+
+    connect.cursor.execute(sql)
+    song_name = connect.cursor.fetchone()
+    connect.close()
+
+    return song_name
+
+
+def create_playing_back_table():
+    connect = ConnectDB()
+
+    sql = '''
+    CREATE TABLE IF NOT EXISTS playing_back(
+    id INTEGER,
+    song VARCHAR(100),
+    PRIMARY KEY(id AUTOINCREMENT)
+    )
+    '''
+    connect.cursor.execute(sql)
+    connect.close()
+
+
+def create_playing_next_table():
+    connect = ConnectDB()
+
+    sql = '''
+    CREATE TABLE IF NOT EXISTS playing_next(
+    id INTEGER,
+    song VARCHAR(100),
+    PRIMARY KEY(id AUTOINCREMENT)
+    )
+    '''
+    connect.cursor.execute(sql)
+    connect.close()
+
+
+def add_to_playing_next(play_next_song):
+    connect = ConnectDB()
+
+    sql = f'''INSERT INTO playing_next(song) VALUES ('{play_next_song}')'''
+    print(play_next_song)
+
+    connect.cursor.execute(sql)
+    connect.close()
+
+
+def add_to_playing_back(play_back_song):
+    connect = ConnectDB()
+
+    sql = '''INSERT INTO playing_back(song) VALUES (?)'''
+    play_back_song = str(play_back_song)
+    play_back_song = play_back_song.strip("()")
+    play_back_song = play_back_song.strip(",")
+    play_back_song = play_back_song.strip("'")
+    play_back_song = play_back_song.strip()
+    print(play_back_song)
+    connect.cursor.execute(sql, (play_back_song,))
+    connect.close()
+
+
+def play_next():
+    connect = ConnectDB()
+
+    sql = '''SELECT song
+        FROM playing_next
+        ORDER BY id
+        LIMIT 1'''
+
+    connect.cursor.execute(sql)
+    playing_next_song = connect.cursor.fetchone()
+
+    sql_delete = '''
+        DELETE FROM playing_next
+        WHERE id = (
+        SELECT id
+        FROM playing_next
+        ORDER BY id
+        LIMIT 1)
+    '''
+
+    connect.cursor.execute(sql_delete)
+    connect.close()
+
+    add_to_playing_back(f'{playing_next_song}')
+
+    return playing_next_song
+
+
+def play_back():
+    connect = ConnectDB()
+
+    sql = ''' 
+        SELECT song
+        FROM playing_back
+        ORDER BY song
+        LIMIT 1
+    '''
+
+    connect.cursor.execute(sql)
+    playing_back_song = connect.cursor.fetchone()
+
+    sql_delete = '''
+        DELETE FROM playing_back
+        WHERE id = (
+        SELECT id
+        FROM playing_next
+        ORDER BY id
+        LIMIT 1)
+    '''
+
+    connect.cursor.execute(sql_delete)
+    connect.close()
+
+    add_to_playing_next(playing_back_song)
+
+    return playing_back_song
